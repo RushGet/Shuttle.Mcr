446a97319c2dbd27b29a8dbbdb6e3987ef9d7f0d
Newbe36524 <newbe36524@qq.com>
2023-03-04 17:07:08 +0800

    1

--- tasks.py
+++ tasks.py
@@ -1,50 +1,156 @@
+import json
+import logging
+import os
 import re
+from enum import Enum
+
+import requests
 import yaml
 from invoke import task
-import logging
+import shutil
+
+from models import *
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
 
 
-def load_config():
+def get_all_tags(registry: DockerRegistry, image: str):
+    if registry == DockerRegistry.MCR:
+        url = f"https://mcr.microsoft.com/v2/{image}/tags/list"
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()['tags']
+    elif registry == DockerRegistry.DOCKERHUB:
+        # get token
+        r = requests.get(f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:{image}:pull")
+        token = r.json()['token']
+        # get tags
+        url = f"https://index.docker.io/v2/{image}/tags/list"
+        r = requests.get(url, headers={"Authorization": f"Bearer {token}"})
+        if r.status_code == 200:
+            return r.json()['tags']
+    else:
+        url = f"https://{registry.value}/v2/{image}/tags/list"
+        r = requests.get(url)
+        if r.status_code == 200:
+            return r.json()['tags']
+
+
+def load_config() -> ShuttleConfig:
     # Load config from file
     path = "config/config.yaml"
     with open(path, "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    return config
+    return ShuttleConfig.from_yaml(config)
 
 
-def select_mcr_tags(config, mcr_tags_json):
-    image_name = mcr_tags_json['name']
-    # find image selector from config where selector[].image match mar_tags_json.name
-    image_selector_config = [x for x in config['selectors'] if (x['image'] == image_name and x['type'] == 'mcr')]
-    if len(image_selector_config) == 0:
-        logging.warning(f"No image selector found for image: {image_name}")
-        return None
-    elif len(image_selector_config) > 1:
-        logging.warning(f"Multiple image selectors found for image: {image_name}")
-        return None
-    else:
-        image_selector_config = image_selector_config[0]
-        # test each tags in mcr_tags_json.tags by selector[].tag_regex and selector[].tag_regex_exclude
-        # if tag_regex is not defined, all tags are selected
-        # if tag_regex_exclude is defined, tags matching tag_regex_exclude are excluded
-        results = []
-        for tag in mcr_tags_json['tags']:
-            if 'tag_regex' in image_selector_config:
-                for tag_regex in image_selector_config['tag_regex']:
-                    if re.match(tag_regex, tag):
-                        if 'tag_regex_exclude' in image_selector_config:
-                            for tag_regex_exclude in image_selector_config['tag_regex_exclude']:
-                                if re.match(tag_regex_exclude, tag):
-                                    break
-                            else:
-                                results.append(tag)
-                        else:
-                            results.append(tag)
+def select_mcr_tags(image_selector_config: ShuttleImageConfig, tags: list[str]) -> list[McrTagsMatchingResult]:
+    image_name = image_selector_config.name
+    results = []
+    selected_tags = []
+    for tag in tags:
+        if match_tag_by_regex(tag, image_selector_config.tag_regex,
+                              image_selector_config.tag_regex_exclude):
+            selected_tags.append(tag)
+    results.append(McrTagsMatchingResult(image_name, image_selector_config, selected_tags))
+    return results
+
+
+def match_tag_by_regex(tag: str, regex_list: list, regex_exclude_list: list = None) -> bool:
+    # test tag by regex_list and regex_exclude_list
+    # if regex_list is not defined, all tags are selected
+    # if regex_exclude_list is defined, tags matching regex_exclude_list are excluded
+    for regex in regex_list:
+        if re.match(regex, tag):
+            if regex_exclude_list is not None:
+                for regex_exclude in regex_exclude_list:
+                    if re.match(regex_exclude, tag):
                         break
-        return results
+                else:
+                    return True
+            else:
+                return True
+    return False
+
+
+def create_image_sync_data_json(items: list[ImageTransportation]) -> list[ImageSyncData]:
+    # create json for image-syncer
+    limit_tags_count = 10
+    data = []
+    for item in items:
+        # spilt tags into chunks, each chunk has limit_tags_count tags
+        tag_chunks = [item.tags[i:i + limit_tags_count] for i in range(0, len(item.tags), limit_tags_count)]
+        for i in range(len(tag_chunks)):
+            tag_chunk = tag_chunks[i]
+            content = {}
+            for tag in tag_chunk:
+                # create json for each chunk
+                source = f"{item.source_registry}/{item.source_image}:{tag}"
+                target = f"{item.target}:{tag}"
+                content[source] = target
+            # write json to file
+            data.append(ImageSyncData(f"{item.source_image.replace('/', '_')}-{i}", content))
+    return data
+
+
+def create_image_sync_data_mcr(config: ShuttleConfig, images: list[str]) -> list[ImageSyncData]:
+    for source_image in images:
+        url = f"https://mcr.microsoft.com/v2/{source_image}/tags/list"
+        r = requests.get(url)
+        if r.status_code == 200:
+            match_results = select_mcr_tags(config, r.json())
+            # map match_results to ImageTransportation
+            items = []
+            for match_result in match_results:
+                for tag in match_result.tags:
+                    items.append(
+                        ImageTransportation(source_image, DockerRegistry.MCR.value, match_result.match_config.target,
+                                            tag))
+
+            # create json for image-syncer
+            data = create_image_sync_data_json(items)
 
 
 @task
-def hello(c):
+def create_data(c):
+    # force reset data dir
+    if os.path.exists("data"):
+        shutil.rmtree("data")
+        logging.debug("data dir removed")
+    os.mkdir("data")
+    logging.debug("data dir created")
+
     config = load_config()
-    print(config)
+    for image_config in config.images:
+        logging.info(f"Processing {image_config.image}")
+        tags = get_all_tags(image_config.docker_registry, image_config.image)
+        match_results = select_mcr_tags(image_config, tags)
+        # map match_results to ImageTransportation
+        items = []
+        for match_result in match_results:
+            items.append(
+                ImageTransportation(image_config.docker_registry.value,
+                                    image_config.image,
+                                    image_config.target,
+                                    match_result.tags))
+
+        # create json for image
+        data = create_image_sync_data_json(items)
+
+        # write json to file
+        for item in data:
+            logging.info(f"Writing {item.name}.json")
+            with open(f"data/{item.name}.json", "w") as f:
+                json.dump(item.items, f, indent=4)
+
+
+@task(pre=[create_data])
+def sync(c):
+    # sync images
+    for file in os.listdir("data"):
+        if file.endswith(".json"):
+            c.run(f"./image-syncer --auth=./auth.json --images=./data/{file}")
--- tests.py
+++ tests.py
@@ -1,25 +1,100 @@
-import json
 import unittest
 
-from tasks import load_config, select_mcr_tags
+from tasks import *
 
 
 class MyTestCase(unittest.TestCase):
     def test_load_config(self):
         config = load_config()
         self.assertIsNotNone(config)
-        self.assertEqual(config['version'], 0.1)
+        self.assertEqual(config.version, 0.1)
         print(config)
 
     def test_filter_by_tag_selectors(self):
         config = load_config()
         with open('test_data/dotnet_sdk_tags.json', "r", encoding='utf-8') as f:
             mcr_tags_json = json.load(f)
-        tags = select_mcr_tags(config, mcr_tags_json)
-        self.assertIsNotNone(tags)
+        result = select_mcr_tags(config.images[0], mcr_tags_json['tags'])
+        self.assertIsNotNone(result)
         with open('test_data/dotnet_sdk_match_data.json', "r", encoding='utf-8') as f:
             match_data = json.load(f)
-        self.assertEqual(tags, match_data)
+        print(result[0])
+        # check tags
+        self.assertEqual(result[0].tags, match_data)
+
+    def test_filter_by_tag_selectors_redis(self):
+        config = load_config()
+        with open('test_data/redis_dockerhub_tags.json', "r", encoding='utf-8') as f:
+            mcr_tags_json = json.load(f)
+        result = select_mcr_tags(config.images[1], mcr_tags_json['tags'])
+        print(result)
+        self.assertIsNotNone(result)
+        with open('test_data/redis_match_data.json', "r", encoding='utf-8') as f:
+            match_data = json.load(f)
+        print(result[0])
+        # check tags
+        self.assertEqual(result[0].tags, match_data)
+
+
+class McrTagMatchTests(unittest.TestCase):
+    def test_match_include(self):
+        tag = '3.1.201'
+        regex_list = ['^3\\..*']
+        regex_exclude_list = None
+        self.assertTrue(match_tag_by_regex(tag, regex_list, regex_exclude_list))
+
+    def test_match_exclude(self):
+        tag = '3.1.201-preview1'
+        regex_list = ['^3\\..*']
+        regex_exclude_list = ['.*preview.*']
+        self.assertFalse(match_tag_by_regex(tag, regex_list, regex_exclude_list))
+
+
+class ImageSyncDataTests(unittest.TestCase):
+    def test_create_image_sync_data_json(self):
+        items = [
+            ImageTransportation('mcr.microsoft.com',
+                                'dotnet/sdk',
+                                'registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk',
+                                ['3.1.201', '3.1.202'])]
+        result = create_image_sync_data_json(items)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 1)
+        expected = {
+            "mcr.microsoft.com/dotnet/sdk:3.1.201": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:3.1.201",
+            "mcr.microsoft.com/dotnet/sdk:3.1.202": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:3.1.202"
+        }
+        self.assertEqual(expected, result[0].items)
+        self.assertEqual('dotnet_sdk-0', result[0].name)
+        logging.info(result[0])
+
+    def test_create_image_sync_data_json_with_more_tags(self):
+        items = [
+            ImageTransportation('mcr.microsoft.com',
+                                'dotnet/sdk',
+                                'registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk',
+                                ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'])]
+        result = create_image_sync_data_json(items)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 2)
+        expected0 = {
+            "mcr.microsoft.com/dotnet/sdk:1": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:1",
+            "mcr.microsoft.com/dotnet/sdk:2": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:2",
+            "mcr.microsoft.com/dotnet/sdk:3": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:3",
+            "mcr.microsoft.com/dotnet/sdk:4": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:4",
+            "mcr.microsoft.com/dotnet/sdk:5": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:5",
+            "mcr.microsoft.com/dotnet/sdk:6": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:6",
+            "mcr.microsoft.com/dotnet/sdk:7": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:7",
+            "mcr.microsoft.com/dotnet/sdk:8": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:8",
+            "mcr.microsoft.com/dotnet/sdk:9": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:9",
+            "mcr.microsoft.com/dotnet/sdk:10": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:10"
+        }
+        self.assertEqual(result[0].items, expected0)
+
+        expected1 = {
+            "mcr.microsoft.com/dotnet/sdk:11": "registry.cn-hangzhou.aliyuncs.com/newbe36524/dotnet-sdk:11"
+        }
+        self.assertEqual(result[1].items, expected1)
 
 
 if __name__ == '__main__':
